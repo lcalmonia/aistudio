ea3547f852d1c78d8e0decea6fd82779495a285a
lcalmonia <166729703+lcalmonia@users.noreply.github.com>
2026-09-03 00:39:34 +0800

    Remove unnecessary bundle type augmentation

--- src/types.bundle.d.ts
+++
@@ -1,8 +0,0 @@
-import './types';
-
-declare module './types' {
-  interface CustomerCartItem {
-    /** Customized products selected for this combo, kept with the bundle cart line. */
-    bundleSelections?: CustomerCartItem[];
-  }
-}
